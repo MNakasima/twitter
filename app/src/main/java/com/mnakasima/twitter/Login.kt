876731f065cf78c38f10b4d015074a6393f858c2
Maruko <marco.k95@hotmail.com>
2019-11-27 10:24:28 -0300

    load images to login profile

--- app/src/main/java/com/mnakasima/twitter/Login.kt
+++ app/src/main/java/com/mnakasima/twitter/Login.kt
@@ -1,8 +1,16 @@
 package com.mnakasima.twitter
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
@@ -12,11 +20,69 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         ivProfile.setOnClickListener( View.OnClickListener {
-            //TODO later
+            checkPermission()
+
         })
     }
 
     fun buLoginEvent(view: View){
 
     }
+
+    val PICK_IMAGE_CODE = 123
+    fun loadImage(){
+
+        var intent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        startActivityForResult(intent, PICK_IMAGE_CODE)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == PICK_IMAGE_CODE && data != null && resultCode == Activity.RESULT_OK){
+            val selectedImage = data.data
+            val filePathColum = arrayOf(MediaStore.Images.Media.DATA)
+            val cursor = contentResolver.query(selectedImage!!, filePathColum, null , null ,null)
+            cursor!!.moveToFirst()
+            val columnIndex = cursor.getColumnIndex(filePathColum[0])
+            val picturePath = cursor.getString(columnIndex)
+            cursor.close()
+            ivProfile.setImageBitmap(BitmapFactory.decodeFile(picturePath))
+        }
+    }
+
+
+    val PERMISSION_CODE = 321
+    fun checkPermission(){
+
+        if(Build.VERSION.SDK_INT >= 23){
+            if(ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_CODE)
+                return
+            }
+        }
+        loadImage()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+
+        when (requestCode){
+            PERMISSION_CODE -> {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    loadImage()
+                }else{
+                    Toast.makeText(this, "Cannot access your images", Toast.LENGTH_LONG).show()
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
 }
