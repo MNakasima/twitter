da216df82cefa9b39193e1db77a6a3c86ecf19bc
Maruko <marco.k95@hotmail.com>
2019-12-02 12:58:42 -0300

    add post

--- app/src/main/java/com/mnakasima/twitter/MainActivity.kt
+++ app/src/main/java/com/mnakasima/twitter/MainActivity.kt
@@ -1,21 +1,46 @@
 package com.mnakasima.twitter
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.add_ticket.*
+import kotlinx.android.synthetic.main.add_ticket.view.*
+import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     var listTweet = arrayListOf<Ticket>()
     var adapter:MyTweetAdapter?=null
+    var myEmail:String?=null
+    var userUID:String?=null
+
+    private var database = FirebaseDatabase.getInstance()
+    private var myRef = database.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var bundle:Bundle? = intent.extras
+        myEmail = bundle!!.getString("email")
+        userUID = bundle!!.getString("uid")
 
         listTweet.add(Ticket("0","him","url","add"))
         listTweet.add(Ticket("1","him","url","maruko"))
@@ -41,7 +66,22 @@ class MainActivity : AppCompatActivity() {
             if (myTweet.personUID.equals("add")){
                 //Load add ticket
                 var myView = layoutInflater.inflate(R.layout.add_ticket, null)
+
+                myView.iv_attach.setOnClickListener(View.OnClickListener {
+
+                    loadImage()
+
+                })
+
+                myView.iv_post.setOnClickListener( View.OnClickListener {
+
+                    myRef.child("posts").push().setValue(
+                        PostInfo( userUID!!, myView.etPost.text.toString() , downloadURL!!))
+                    myView.etPost.setText("")
+
+                })
                 return myView
+                
             }else{
                 //Load tweet ticket
                 var myView = layoutInflater.inflate(R.layout.tweets_ticket, null)
@@ -62,5 +102,65 @@ class MainActivity : AppCompatActivity() {
             return listTweetAdapter.size
         }
 
+    }
+
+    val PICK_IMAGE_CODE = 123
+    fun loadImage(){
+
+        var intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+        startActivityForResult(intent, PICK_IMAGE_CODE)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == PICK_IMAGE_CODE && data != null && resultCode == Activity.RESULT_OK){
+            val selectedImage = data.data
+            val filePathColum = arrayOf(MediaStore.Images.Media.DATA)
+            val cursor = contentResolver.query(selectedImage!!, filePathColum, null , null ,null)
+            cursor!!.moveToFirst()
+            val columnIndex = cursor.getColumnIndex(filePathColum[0])
+            val picturePath = cursor.getString(columnIndex)
+            cursor.close()
+            uploadImage(BitmapFactory.decodeFile(picturePath))
+        }
+    }
+
+
+    var downloadURL:String?=null
+    fun uploadImage(bitmap:Bitmap){
+
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.getReferenceFromUrl("gs://twitter-6f828.appspot.com")
+
+        val df = SimpleDateFormat("ddMMyyHHmmss")
+        val dataobj = Date()
+
+        val imagePath = splitSting(myEmail!!) +"."+df.format(dataobj)+ ".jpg"
+        val imageRef = storageRef.child("ImagesPost/"+imagePath)
+
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        val uploadTask= imageRef.putBytes(data)
+
+        uploadTask.addOnFailureListener{
+
+            Toast.makeText(applicationContext,"fail to upload image", Toast.LENGTH_LONG).show()
+
+        }.addOnSuccessListener {
+
+                taskSnapshot ->
+            downloadURL = taskSnapshot.metadata!!.reference!!.downloadUrl.toString()
+
+        }
+    }
+
+    fun splitSting(email:String):String{
+        val split =  email.split("@")
+        return split[0]
     }
 }
